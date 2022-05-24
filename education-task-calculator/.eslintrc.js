f57bcced2510b66197bce1a7cf1723e5a6256408
ruslan <ruslan.abdallah2000@gmail.com>
2022-05-24 19:14:13 +0300

    init: base files

---
+++ education-task-calculator/.eslintrc.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+  },
+  extends: ['react-app', 'prettier'],
+
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+  },
+};
